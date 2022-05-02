47436e86bd34dfbb7398a68c13749cd6be638c7c
jotish80 <sjotishchandra@gmail.com>
2022-05-02 17:38:53 +0200

    first commit server set up done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middle ware
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/', (req,res) =>{
+    res.send('running my assignment server')
+});
+
+
+app.listen(port, () =>{
+    console.log("listening to the port", port);
+})
